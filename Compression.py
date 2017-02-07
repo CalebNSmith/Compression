e5cf0fd7a4fb52fd7279df57f9f373ddcdb2b0cb
CalebNSmith <Caleb@Calebs-MacBook-Pro.local>
2017-02-07 08:36:05 -0500

    More comments

--- Compression.py
+++ Compression.py
@@ -1,19 +1,20 @@
 from __future__ import generators
 import Points
 import math
+from scipy.optimize import linprog
+
 
 # Given a point, the left-most point, the bottom-most point, and epsilon given a point back corresponding to grid slot
 # e.g. given (3, 5) would mean grid[3][5]
 def find_bucket(point, left, bottom, epsilon):
-    """[summary]
-    
-    [description]
+    """Returns the array indices that a point belongs to after generating the grid
+
     
     Arguments:
-        point {[type]} -- [description]
-        left {[type]} -- [description]
-        bottom {[type]} -- [description]
-        epsilon {[type]} -- [description]
+        point {list of integers} -- the point from Points.py that you want to find the grid slot for
+        left {list of integers} -- The leftmost point in the dataset, retrived from Points.get_left_point()
+        bottom {list of integers} -- The bottommost point in the dataset, retrived from Points.get_bottom_point
+        epsilon {float} -- Degree of error
     """
     return [math.floor(point[0] - (left[0] - 4 * epsilon)), math.floor(point[1] - (bottom[1] - 4 * epsilon))]
 
@@ -49,8 +50,24 @@ def extend_left(point1, point2, epsilon, points, grid):
                     if math.sqrt((point[1] - point3[1]) ** 2 + (point[0] - point3[0]) ** 2) <= 8 * epsilon:
                         points_found.append(point3)
             except IndexError:
-                continue
-    return points_found
+                continue    points_found = [point1, point2]
+    more_points = True
+    while(more_points):
+        first = points_found[len(points_found - 1)]
+        second = points_found[len(points_found - 2)]
+        x_difference = first[0] - second[0]
+        if x_difference == 0:
+            x_difference = 1
+        slope = float((second[1] - first[1]) / x_difference)
+        point3 = [second[0] - math.fabs(x_difference), slope * x_difference + second[1]]
+        bucket = find_bucket(point3, Points.get_left_point(points), Points.get_bottom_point(points), epsilon)
+        distance = [math.fabs(first[0] - second[0]), math.fabs(first[1] - second[1])]
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                try:
+
+                except IndexError:
+                    continue    return points_found
 
 def extend_right(point1, point2, epsilon, points, grid):
     """[summary]
@@ -86,36 +103,33 @@ def extend_right(point1, point2, epsilon, points, grid):
     return points_found
 
 def check_distance(points, epsilon):
-    """[summary]
+    """Returns true if any two points are within 8*epsilon of each other
     
-    [description]
     
     Arguments:
-        points {[type]} -- [description]
-        epsilon {[type]} -- [description]
+        points {list of a list of integers} -- List of points from Points.py
+        epsilon {float} -- Degree of error
     
     Returns:
-        bool -- [description]
+        bool -- If all of the points are >= 8*epsilon apart, return false; Otherwise return true
     """
     for point1 in points:
         for point2 in points:
             if point1 == point2:
                 continue
-            if distance(point1, point2) <= 8 * epsilon:
+            if distance(point1, point2) < 8 * epsilon:
                 return True
     return False
 
 def distance(point1, point2):
-    """[summary]
-    
-    [description]
+    """Distance between two points
     
     Arguments:
-        point1 {[type]} -- [description]
-        point2 {[type]} -- [description]
+        point1 {list of integers} -- 
+        point2 {list of integers} -- 
     
     Returns:
-        [type] -- [description]
+        float -- distance between the two points
     """
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
@@ -183,3 +197,16 @@ for first in points:
         if len(subsequence) > 2 and subsequence not in sequences:
             sequences.append(subsequence)
 print(sequences)
+
+print("\n\n\n\n\n")
+c = []
+A = [[-3, 1], [1, 2]]
+b = [6, 4]
+x0_bounds = (None, None)
+x1_bounds = (-3, None)
+res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
+              options={"disp": False})
+
+
+
+print(res)
